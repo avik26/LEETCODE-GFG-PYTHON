2b421036e7dd07b8b81db52b7a6e7e9602dac32f
Avik Ghosh <130585622+avik26@users.noreply.github.com>
2023-10-13 01:04:20 +0530

    Added solution - LeetHub

---
+++ Reverse array in groups - GFG/reverse-array-in-groups.py
@@ -0,0 +1,42 @@
+#User function template for Python
+
+class Solution:
+    # Function to reverse every sub-array group of size K.
+    def reverseInGroups(self, arr, N, K):
+        for i in range(0, N, K):
+            left = i
+            right = min(i + K - 1, N - 1)
+
+            while left < right:
+                arr[left], arr[right] = arr[right], arr[left]
+                left += 1
+                right -= 1
+
+
+#{ 
+ # Driver Code Starts
+#Initial template for Python
+
+import math
+def main():
+    T=int(input())
+    while(T>0):
+        nk=[int(x) for x in input().strip().split()]
+        N=nk[0]
+        K=nk[1]
+        arr=[int(x) for x in input().strip().split()]
+        
+        ob = Solution()
+        ob.reverseInGroups(arr,N,K)
+        for i in arr:
+            print(i,end=" ")
+        print()
+        T-=1
+
+if __name__=="__main__":
+    main()
+
+
+
+
+# } Driver Code Ends
