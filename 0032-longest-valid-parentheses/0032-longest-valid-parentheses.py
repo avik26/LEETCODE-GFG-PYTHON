8fb249351c90ecd910b906561a3c2ab7a60feb66
Avik Ghosh <130585622+avik26@users.noreply.github.com>
2023-10-13 10:16:24 +0530

    Time: 44 ms (79.41%), Space: 17.1 MB (32.98%) - LeetHub

---
+++ 0032-longest-valid-parentheses/0032-longest-valid-parentheses.py
@@ -0,0 +1,26 @@
+class Solution:
+    def longestValidParentheses(self, s: str) -> int:
+        stack = []  # Stack to store the indices of open parentheses
+        max_length = 0  # Maximum length of valid parentheses
+
+        # Initialize a variable to keep track of the last invalid index
+        last_invalid = -1
+
+        for i, char in enumerate(s):
+            if char == '(':
+                stack.append(i)
+            else:
+                if not stack:
+                    # If the stack is empty, the current ')' is invalid
+                    last_invalid = i
+                else:
+                    # Pop the last open parenthesis
+                    stack.pop()
+                    if not stack:
+                        # If the stack is empty, the substring from last_invalid to i is valid
+                        max_length = max(max_length, i - last_invalid)
+                    else:
+                        # If the stack is not empty, the substring from the last element in the stack to i is valid
+                        max_length = max(max_length, i - stack[-1])
+
+        return max_length
