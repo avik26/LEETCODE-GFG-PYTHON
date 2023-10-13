ddb0c9edd1862c73182a36ae3e0245e21fa3f46a
Avik Ghosh <130585622+avik26@users.noreply.github.com>
2023-10-13 10:18:38 +0530

    Time: 291 ms (20.33%), Space: 17.5 MB (90.46%) - LeetHub

--- 1540-can-convert-string-in-k-moves/1540-can-convert-string-in-k-moves.py
+++ 1540-can-convert-string-in-k-moves/1540-can-convert-string-in-k-moves.py
@@ -3,15 +3,12 @@ class Solution:
         if len(s) != len(t):
             return False
         
-        shift_count = {}  # Dictionary to store the number of shifts needed for each character
+        shift_count = [0] * 26  # An array to store the number of shifts needed for each possible shift
         
         for i in range(len(s)):
             diff = (ord(t[i]) - ord(s[i]) + 26) % 26  # Calculate the shift, considering wrapping
             if diff != 0:
-                if diff not in shift_count:
-                    shift_count[diff] = 1
-                else:
-                    shift_count[diff] += 1
+                shift_count[diff] += 1
                 
                 # Check if the total shift is within the allowed limit
                 if diff + (shift_count[diff] - 1) * 26 > k:
