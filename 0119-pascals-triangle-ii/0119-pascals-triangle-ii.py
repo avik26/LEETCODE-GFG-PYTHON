e9a76035fb37a0850c08c13a143ac5494d31352f
Avik Ghosh <130585622+avik26@users.noreply.github.com>
2023-10-16 10:28:29 +0530

    Time: 48 ms (6.25%), Space: 16.1 MB (82.18%) - LeetHub

--- 0119-pascals-triangle-ii/0119-pascals-triangle-ii.py
+++ 0119-pascals-triangle-ii/0119-pascals-triangle-ii.py
@@ -1,20 +1,9 @@
-from typing import List
-
 class Solution:
-    def getRow(self, rowIndex: int) -> List[int]:
-        if rowIndex < 0:
-            return []
-        
-        # Initialize the first row with 1.
-        row = [1]
-        
-        for i in range(1, rowIndex + 1):
-            # Calculate each element in the current row.
-            new_row = [1]
-            for j in range(1, len(row)):
-                new_row.append(row[j - 1] + row[j])
-            new_row.append(1)
-            
-            row = new_row  # Update the current row with the new row.
-        
-        return row
+  def getRow(self, rowIndex: int) -> List[int]:
+    ans = [1] * (rowIndex + 1)
+
+    for i in range(2, rowIndex + 1):
+      for j in range(1, i):
+        ans[i - j] += ans[i - j - 1]
+
+    return ans
