3b9e85628aef682456ab5e159609425861789c8c
Avik Ghosh <130585622+avik26@users.noreply.github.com>
2023-10-17 10:18:17 +0530

    Time: 44 ms (44.53%), Space: 16.2 MB (51.10%) - LeetHub

--- 0202-happy-number/0202-happy-number.py
+++ 0202-happy-number/0202-happy-number.py
@@ -1,19 +1,23 @@
 class Solution:
     def isHappy(self, n: int) -> bool:
         def get_next(num):
-            # Helper function to calculate the sum of the squares of digits.
             total_sum = 0
             while num > 0:
                 num, digit = divmod(num, 10)
                 total_sum += digit ** 2
             return total_sum
 
-        seen = set()
-        while n != 1 and n not in seen:
-            seen.add(n)
-            n = get_next(n)
+        slow = n
+        fast = n
 
-        return n == 1
+        while True:
+            slow = get_next(slow)  # Move one step.
+            fast = get_next(get_next(fast))  # Move two steps.
+
+            if fast == 1:
+                return True  # If fast reaches 1, it's a happy number.
+            if slow == fast:
+                return False  # If there's a cycle (other than 1), it's not a happy number.
 
 # Example usage:
 # n = 19
