5a44325f8084dc6b5df65f09013aff1230678ae8
Avik Ghosh <130585622+avik26@users.noreply.github.com>
2024-02-18 23:00:52 +0530

    Update 0011-container-with-most-water.py

--- 0011-container-with-most-water/0011-container-with-most-water.py
+++ 0011-container-with-most-water/0011-container-with-most-water.py
@@ -1,5 +1,5 @@
 class Solution:
-    def maxArea(self, height: List[int]) -> int:
+    def maxArea(self, height: List[int]) -> int: 
         max_area = 0
         left = 0
         right = len(height) - 1
